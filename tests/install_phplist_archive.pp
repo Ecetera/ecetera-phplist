0ecb0e18d1215c18016932bec7f5ee189b7a256f
Issa Moussa <issa.moussa@ecetera.com.au>
2013-07-21 22:29:11 +0000

    Fixed install with RPM

--- tests/install_phplist_archive.pp
+++ tests/install_phplist_archive.pp
@@ -1,32 +1,20 @@
-# The baseline for module testing used by Puppet Labs is that each manifest
-# should have a corresponding test manifest that declares that class or defined
-# type.
-#
-# Tests are then run by using puppet apply --noop (to check for compilation errors
-# and view a log of events) or by fully applying the test in a virtual environment
-# (to compare the resulting system state to the desired state).
-#
-# Learn more about module testing here: http://docs.puppetlabs.com/guides/tests_smoke.html
-#
 # Test a standard phplist installation with an archive
 
-class { 'apache': 
-} 
-
+# Install dependencies
+class { 'apache': }
 class { 'apache::mod::php': }
 
 
-# TODO : Use a generic name to identify the phplist and set a link to default value.
-class { 'phplist': 
- version      => '2.11.9',
- install_dir  => '/var/www/html/phplist',
- config_dir   => '/var/www/html/phplist/phplist-2.11.9/public_html/lists/config',
- install_type => 'tar',
- url          => 'http://download.example.com/phplist-2.11.9.tar.gz',
-} -> 
+class { 'phplist':
+version      => '2.11.9',
+install_dir  => '/var/www/html/phplist',
+config_dir   => '/var/www/html/phplist/phplist-2.11.9/public_html/lists/config',
+install_type => 'tar',
+url          => 'http://download.example.com/phplist-2.11.9.tar.gz',
+} ->
 apache::vhost { 'testphplist.example.com':
-	priority   => '10',
-	vhost_name => '*',
-	port       => '80',
-	docroot    => '/var/www/html/phplist',
+  priority   => '10',
+  vhost_name => '*',
+  port       => '80',
+  docroot    => '/var/www/html/phplist',
 }
--- tests/multisite.pp
+++ tests/multisite.pp
@@ -1,27 +1,19 @@
-# The baseline for module testing used by Puppet Labs is that each manifest
-# should have a corresponding test manifest that declares that class or defined
-# type.
-#
-# Tests are then run by using puppet apply --noop (to check for compilation errors
-# and view a log of events) or by fully applying the test in a virtual environment
-# (to compare the resulting system state to the desired state).
-#
-# Learn more about module testing here: http://docs.puppetlabs.com/guides/tests_smoke.html
-#
+# Test the multisite installation
+# TODO work in progress. Not ready yet
 include phplist
 include apache
-# TODO : Use a generic name to identify the phplist and set a link to default value.
-	apache::vhost { 'testphplist.example.com':
-		priority   => '10',
-		vhost_name => '*',
-		port       => '80',
-		docroot    => '/phplist-2.11.9/public_html',
-    require    => Class['phplist']
-	}  
+
+apache::vhost { 'testphplist.example.com':
+  priority   => '10',
+  vhost_name => '*',
+  port       => '80',
+  docroot    => '/phplist-2.11.9/public_html',
+  require    => Class['phplist']
+  }
 
   $directories = ['/var/www/html/newsletter', '/var/www/html/newsletter/public_html', '/var/www/html/newsletter/public_html/lists' ]
 
-    file { $directories: 
-      ensure => 'directory',
-      require => [ Class['apache']]
-    }
+  file { $directories:
+  ensure => 'directory',
+  require => [ Class['apache']]
+  }
